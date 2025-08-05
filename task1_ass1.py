23f7490bd49ef2eb0774e92b672a1214492a361a
Ankit6174-cmd <tiwari.ankit225@gmail.com>
2025-08-05 16:15:52 +0530

    Add files via upload

---
+++ task1_ass1.py
@@ -0,0 +1,16 @@
+# defining the two numbers
+a=float(input("enter the first number="))
+b=float(input("enter the second number="))
+#defining the variables to assign the operation and print them
+c=a+b
+c=float(c)
+print("addition =",c)
+d=a-b
+d=float(d)
+print("subtraction =",d)
+e=a*b
+e=float(e)
+print("multiplication =",e)
+f=a/b
+f=float(f)
+print("division =",f)
